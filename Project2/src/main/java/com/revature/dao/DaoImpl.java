a01457e3ac095521022a9168407bb69de67ed5af
Allen Arce <a.jarce@yahoo.com>
2017-10-27 13:08:24 -0400

    added some dao methods

--- Project2/src/main/java/com/revature/dao/DaoImpl.java
+++ Project2/src/main/java/com/revature/dao/DaoImpl.java
@@ -1,5 +1,38 @@
 package com.revature.dao;
 
-public class DaoImpl implements Dao{
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import com.revature.beans.ScrumUser;
+import com.revature.beans.Task;
+import com.revature.util.HibernateConnectionUtil;
+
+public class DaoImpl implements Dao{
+	//Get an Existing Scrum User from the DB
+		public ScrumUser getScrumUserById(ScrumUser sUser) {
+			Session session = HibernateConnectionUtil.getSession();
+			ScrumUser dbUser = (ScrumUser) session.get(ScrumUser.class, sUser.getScrumUerId());
+			session.close();
+			return dbUser;
+		}
+		
+		//Create a new Task and Save it to the DB
+		@Override
+		public void createTask(Task t) {
+			Session session = HibernateConnectionUtil.getSession();
+			Transaction tx = session.beginTransaction();
+			session.save(t);
+			tx.commit();
+			session.close();
+		}
+		
+		//Get an Existing Task from the DB
+		//Can delete this if not needed
+		@Override
+		public Task getTaskById(Task t) {
+			Session session = HibernateConnectionUtil.getSession();
+			Task dbTask = (Task) session.get(Task.class, t.getTaskId());
+			session.close();
+			return dbTask;
+		}
 }
