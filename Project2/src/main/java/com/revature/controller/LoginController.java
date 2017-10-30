d8a16f60964f2bdc931a6ea30db3b10336611bd0
William Deala <wdeala@gmail.com>
2017-10-30 10:23:30 -0400

    Switch JSON parser to Faster Jackson for auto-conversions

--- Project2/src/main/java/com/revature/controller/LoginController.java
+++ Project2/src/main/java/com/revature/controller/LoginController.java
@@ -3,9 +3,9 @@ package com.revature.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,9 +19,8 @@ public class LoginController {
 	@Autowired
 	AppService service;
 
-	@SuppressWarnings("unchecked")
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ResponseEntity<String> login(ScrumUser user, HttpServletRequest request) {
+	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ScrumUser> login(ScrumUser user, HttpServletRequest request) {
 		String username = request.getParameter("username").trim();
 		String password = request.getParameter("password").trim();
 		if (username != null && password != null && username != "" && password != "") {
@@ -32,15 +31,9 @@ public class LoginController {
 				// Store the valid user into the session
 				HttpSession session = request.getSession();
 				session.setAttribute("user", su);
-				JSONObject jsonObject = new JSONObject();
-				jsonObject.put("id", su.getScrumUserId());
-				jsonObject.put("role", su.getRoleId().getRoleName());
-				jsonObject.put("firstname", su.getScrumUserFirstname());
-				jsonObject.put("lastname", su.getScrumUserLastname());
-				jsonObject.put("username", su.getScrumUserUsername());
-				return new ResponseEntity<String>(jsonObject.toJSONString(), HttpStatus.OK);
+				return new ResponseEntity<ScrumUser>(su, HttpStatus.OK);
 			}
 		}
-		return new ResponseEntity<String>("Invalid Login", HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<ScrumUser>(user, HttpStatus.BAD_REQUEST);
 	}
 }
