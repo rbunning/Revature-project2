f17202bc9b3578eeeee33bff01264d79f3134508
Regan Bunning <rtbunning@gmail.com>
2017-10-31 11:11:28 -0400

    RB added aspect functionality

--- Project2/src/main/java/com/revature/aspect/AppAspect.java
+++ Project2/src/main/java/com/revature/aspect/AppAspect.java
@@ -1,5 +1,22 @@
 package com.revature.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class AppAspect {
 
+	@Before("within(com.revature.dao.Dao+)")
+	public void adviseForAllDaoMethods(JoinPoint jp) {
+		System.out.println("before the dao method: " + jp.getSignature().getName());
+	}
+	
+	@Before("within(com.revature.service.AppService)")
+	public void adviseForAllServiceMethods(JoinPoint jp) {
+		System.out.println("before the service method: " + jp.getSignature().getName());
+	}
+	
 }
