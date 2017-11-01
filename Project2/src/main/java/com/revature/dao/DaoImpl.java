4ae1b923a02628a6b1e3a4245fd5d7207e337f26
Regan Bunning <rtbunning@gmail.com>
2017-11-01 17:07:35 -0400

    adding in dao and service functions

--- Project2/src/main/java/com/revature/dao/DaoImpl.java
+++ Project2/src/main/java/com/revature/dao/DaoImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import com.revature.beans.Board;
 import com.revature.beans.BoardUserJoin;
 import com.revature.beans.ScrumUser;
+import com.revature.beans.Story;
 import com.revature.beans.Task;
 
 @Repository
@@ -19,7 +20,7 @@ public class DaoImpl implements Dao {
 	@Autowired
 	SessionFactory sessionFactory;
 
-	// Get an Existing Scrum User from the DB
+	// Get an existing Scrum User from the DB.
 	@Override
 	public ScrumUser getScrumUserById(ScrumUser sUser) {
 		Session session = sessionFactory.getCurrentSession();
@@ -27,7 +28,7 @@ public class DaoImpl implements Dao {
 		return dbUser;
 	}
 
-	// Get an Existing Scrum User from the DB (used for login)
+	// Get an existing Scrum User from the DB (used for login).
 	@Override
 	public ScrumUser getScrumUserByUsername(ScrumUser sUser) {
 		Session session = sessionFactory.getCurrentSession();
@@ -36,13 +37,13 @@ public class DaoImpl implements Dao {
 		return dbUser;
 	}
 
-	// Create a new Task and Save it to the DB
+	// Creates a new Task and save it to the DB.
 	@Override
 	public void createTask(Task t) {
 		sessionFactory.getCurrentSession().save(t);
 	}
 
-	// Get an Existing Task from the DB
+	// Get an existing Task from the DB.
 	// Can delete this if not needed
 	@Override
 	public Task getTaskById(Task t) {
@@ -51,17 +52,26 @@ public class DaoImpl implements Dao {
 		return dbTask;
 	}
 
+	// Creates a new Board and save it to the DB.
 	@Override
-	public Board addBoard(Board newBoard) {
+	public Board createBoard(Board newBoard) {
 		Session session = sessionFactory.getCurrentSession();
 		session.save(newBoard);
 		return newBoard;
 	}
 
+	// Creates a new join between the Board and Scrum User and save it to the DB.
 	@Override
-	public void addUserToBoard(Board board, ScrumUser sUser) {
+	public void createUserToBoard(Board board, ScrumUser sUser) {
 		Session session = sessionFactory.getCurrentSession();
 		BoardUserJoin buj = new BoardUserJoin(board,sUser);
 		session.save(buj);
+	}
+
+	// Creates a new Story and save it to the DB.
+	@Override
+	public void createStory(Story s) {
+		Session session = sessionFactory.getCurrentSession();
+		session.save(s);
 	}
 }
