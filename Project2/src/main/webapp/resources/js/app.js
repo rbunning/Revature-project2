93d59237cbacc9811e2120be9a22c6b368755f78
William Deala <wdeala@gmail.com>
2017-11-02 06:59:57 -0400

    Added navbar link to list boards for testing

--- Project2/src/main/webapp/resources/js/app.js
+++ Project2/src/main/webapp/resources/js/app.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 }
-var scrumUser = {}
+var scrumUser = {};
 
 angular
 		.module('jabrApp', [ 'ngRoute' ])
@@ -15,6 +15,9 @@ angular
 			}).when("/addBoard", {
 				templateUrl : "resources/features/addBoard.html",
 				controller : "addBoardCtrl"
+			}).when("/listBoard", {
+				templateUrl : "resources/features/listBoard.html",
+				controller : "listBoardCtrl"
 			}).otherwise({
 				redirectTo : '/'
 			})
@@ -23,64 +26,72 @@ angular
 		.controller('indexController', function() {
 		})
 
+		.controller('listBoardCtrl', function($scope, $http, $location) {
+			$http.get('listBoards').then(
+					function(response) {
+						$scope.boards = response.data;
+					}, function(response) {
+						console.log(response);
+					});
+		})
+
 		.controller('homeController', function($scope, $location) {
 			$scope.scrumUser = scrumUser;
-			if($scope.scrumUser.roleId.roleId == 2 ){
+			if ($scope.scrumUser.roleId.roleId == 2) {
 				$scope.isScrumMaster = true;
 			}
 			$scope.addABoard = function() {
 				$location.path('/addBoard');
 			}
+			$scope.listBoards = function() {
+				$location.path('/listBoard');
+			}
+			$scope.home = function() {
+				$location.path('/homePage');
+			}
 		})
 
-		.controller(
-				"loginCtrl",
-				function($scope, $http, $location) {
-					$scope.submit = function() {
-						var data = $.param({
-							scrumUserUsername : $scope.username,
-							scrumUserPassword : $scope.password
-						});
-						var config = {
-							headers : {
-								'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8;'
-							}
-						}
-						$http.post('login', data, config).then(
-								function(response) {
-									scrumUser = response.data;
-									$scope.scrumUser = scrumUser;
-									$location.path('/homePage');
-								}, function(response) {
-									$scope.errorMessage = 'Incorrect username or password. Please try again.' ;
-									scrumUser = response;
-									console.log(response);
-								});
-					};
-
-				})
-
-.controller(
-		"addBoardCtrl",
-		function($scope, $http, $location) {
+		.controller("loginCtrl", function($scope, $http, $location) {
 			$scope.submit = function() {
 				var data = $.param({
-					boardName : $scope.boardname
+					scrumUserUsername : $scope.username,
+					scrumUserPassword : $scope.password
 				});
 				var config = {
 					headers : {
 						'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8;'
 					}
 				}
-				$http.post('newBoard', data, config).then(
-						function(response) {
-							$location.path('/homePage');
-						}, function(response) {
-							console.log(response);
-						});
+				$http.post('login', data, config).then(
+					function(response) {
+						scrumUser = response.data;
+						$scope.scrumUser = scrumUser;
+						$location.path('/homePage');
+					},
+					function(response) {
+						$scope.errorMessage = 'Incorrect username or password. Please try again.';
+						scrumUser = response;
+						console.log(response);
+					});
 			};
-
 		})
 
+		.controller("addBoardCtrl",	function($scope, $http, $location) {
+				$scope.submit = function() {
+					var data = $.param({
+						boardName : $scope.boardname
+					});
+					var config = {
+						headers : {
+							'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8;'
+						}
+					}
+					$http.post('newBoard', data, config).then(
+							function(response) {
+								$location.path('/homePage');
+							}, function(response) {
+								console.log(response);
+							});
+				};
 
-
+			})
