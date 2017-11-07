fe3671d29fd4b79388d2c7b422af1b4f32c5f40b
Regan Bunning <rtbunning@gmail.com>
2017-11-07 07:38:25 -0500

    adding in 7 more unit tests

--- Project2/src/main/java/com/revature/test/ServiceTest.java
+++ Project2/src/main/java/com/revature/test/ServiceTest.java
@@ -9,7 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.revature.beans.Board;
 import com.revature.beans.LaneType;
+import com.revature.beans.RoleType;
+import com.revature.beans.ScrumUser;
+import com.revature.beans.Story;
+import com.revature.beans.Task;
 import com.revature.service.AppService;
  
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -20,16 +25,63 @@ public class ServiceTest {
 	AppService service;
 
 	@Test
-	public void logsExist() {
+	public void manyLogsExist() {
 		assertNotNull(service.getLogList());
 	}
 	
 	@Test
-	public void laneExist() {
+	public void manyUsersExist() {
+		assertNotNull(service.getAllUserList());
+	}
+	
+	@Test
+	public void manyLaneTypesExist() {
+		assertNotNull(service.getLaneTypeList());
+	}
+	
+	@Test
+	public void LaneExist() {
 		LaneType testLane = new LaneType(1, null);
-		assertNotNull(service.getLane(testLane));
 		// converts the object to a string because even of the object is the same won't pass the test. 
 		assertEquals(new LaneType(1, "Backlog").toString(), service.getLane(testLane).toString());
 	}
-
+	
+	@Test 
+	public void BoardExist() {
+		Board testBoard = new Board(2);
+		// converts the object to a string because even of the object is the same won't pass the test. 
+		assertEquals(new Board(2, "Allen Test").toString(), service.getBoardDetails(testBoard).toString());
+	}
+	
+	@Test 
+	public void userNotOnBoardExist() {
+		Board testBoard = new Board(2);
+		// converts the object to a string because even of the object is the same won't pass the test. 
+		assertNotNull(service.getUsersNotOnBoard(testBoard));
+	}
+	
+	@Test 
+	public void userOnBoardExist() {
+		Board testBoard = new Board(2);
+		// converts the object to a string because even of the object is the same won't pass the test. 
+		assertNotNull(service.getUserList(testBoard));
+	}
+	
+	@Test
+	public void userExist() {
+		ScrumUser testSu = new ScrumUser(4, null, null, null, null, null);
+		RoleType RT = new RoleType(4, "QA");
+		ScrumUser SU = new ScrumUser(4, RT, "QA", "Test", "qa", "qa");
+		// converts the object to a string because even of the object is the same won't pass the test. 
+		assertEquals(SU.toString(), service.getScrumUserById(testSu).toString());
+	}
+	
+	@Test
+	public void userNameExist() {
+		ScrumUser testSu = new ScrumUser("qa", "qa");
+		RoleType RT = new RoleType(4, "QA");
+		ScrumUser SU = new ScrumUser(4, RT, "QA", "Test", "qa", "qa");
+		// converts the object to a string because even of the object is the same won't pass the test. 
+		assertEquals(SU.toString(), service.getScrumUserByUsername(testSu).toString());
+	}
 }
